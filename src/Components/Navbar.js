2fb847026de5662e8c6cb4f1cc34d564cfa69ef1
Franziska Pendzialek <franziska.pendzialek@outlook.com>
2023-06-18 21:12:47 +0200

    add mortgage calculator

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/system';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Typography variant="h5">The House of your dreams</Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
